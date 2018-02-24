bf9ff9a8a3b782519e28c5d3c9fdd187c17cfd22
Alfre1512 <franalji@hotmail.com>
2018-02-24 11:55:36 -0300

    guardia

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,16 @@
 function Mostrar()
 {
+    var rep= prompt("");
+    var contpares =0 ;
+    for (var yo= rep;yo > 0 ;  yo--){
+        if(yo % 2 ==0){
+            console.log(yo);
+            contpares++;
+
+        
+    }
 
 
+    }
 
-
-}//FIN DE LA FUNCIÓN
+}
