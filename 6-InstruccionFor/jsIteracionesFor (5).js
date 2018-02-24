5c55a03c282c4f3ce1ebbaf8d0ac6f8a86dd6fcd
Alfre1512 <franalji@hotmail.com>
2018-02-24 11:05:48 -0300

    goku

--- 6-InstruccionFor/jsIteracionesFor (5).js
+++ 6-InstruccionFor/jsIteracionesFor (5).js
@@ -1,5 +1,15 @@
 function Mostrar()
 {
+    var num; 
+    for(;num!=9;)
+    num = prompt("");
+    num = parseInt(num);
+    while (isNaN(num))
+    {
+        num = prompt("");
+        num = parseInt( num);
+
+    }
 
 
 
