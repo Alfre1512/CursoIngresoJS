9996d89e229e3312c496cb3d01ec2f136558c79a
Alfre1512 <franalji@hotmail.com>
2018-02-24 12:24:43 -0300

    guardare

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,7 +1,17 @@
 function Mostrar()
 {
+    var rep=prompt("");
+    var contpares  = 0 , msj = "las divisones son";
+    for (var yo =0 ;  yo < rep ; yo++){
+    if (rep % yo == 0){
+        console.log (yo);
+        contpares++;
+        msj = msj + yo ; 
+    }
 
 
 
+    }
 
-}//FIN DE LA FUNCIÓN
+
+}
