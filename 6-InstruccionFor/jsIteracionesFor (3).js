e22f55424165faa90242fd980617a84e21698c40
Alfre1512 <franalji@hotmail.com>
2018-02-24 10:17:12 -0300

    flecha verde

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,9 @@
 function Mostrar()
 {
 
+
 var repetciones = prompt("ingrese el número de repeticiones");
-
-
+for(var yo = 0 ;yo != repetciones ; yo ++)
+    console.log("Hola Utn Fra" + yo)
 
 }//FIN DE LA FUNCIÓN
