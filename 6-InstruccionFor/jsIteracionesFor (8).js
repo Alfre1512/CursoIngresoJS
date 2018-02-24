52f11267a83d56d241d1161cbdf467f4fd823c1c
Alfre1512 <franalji@hotmail.com>
2018-02-24 12:55:48 -0300

    batman

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,14 @@
 function Mostrar()
-{
+ {var num = prompt("");
+var rta ="es primo";
+for (var yo =2  ; yo < num ; yo++){
+    if (num % yo == 0 ) {
+        rta= "No es primo";
+        break;
+    
+    
+    }
+}
+    alert(rta);
 
-
-
-
-}//FIN DE LA FUNCIÓN
+}
